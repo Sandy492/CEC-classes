cecbbd2c0aa45aafee1cf3d4dcef0b8b11d14d22
Sandy492 <75844820+Sandy492@users.noreply.github.com>
2021-02-24 16:48:57 +0530

    Add files via upload

---
+++ pattern10.java
@@ -0,0 +1,19 @@
+/*
+54321
+4321
+321
+21
+1
+*/
+import java.util.Scanner;
+public class pattern10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print(n-j-i);
+            }System.out.println();
+        }
+    }
+}
